bdb8dc3e3c969cebc94f193f25df7789f0388684
Muhammadjon <mamarasulov911@gmail.com>
2023-01-20 21:37:23 +0900

    Initial Commit

---
+++ Task-Ranger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task-Ranger
+//
+//  Created by ericzero on 1/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
